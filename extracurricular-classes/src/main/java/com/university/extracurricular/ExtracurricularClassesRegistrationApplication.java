e0571b1b163a6aa7c71802a968641703c982030b
RubenGarEEP <145350414+RubenGarEEP@users.noreply.github.com>
2024-05-09 16:27:43 +0200

    Creo proyecto + Spring

---
+++ extracurricular-classes/src/main/java/com/university/extracurricular/ExtracurricularClassesRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.university.extracurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtracurricularClassesRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtracurricularClassesRegistrationApplication.class, args);
+	}
+
+}
---
+++ extracurricular-classes/src/test/java/com/university/extracurricular/ExtracurricularClassesRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.extracurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtracurricularClassesRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
