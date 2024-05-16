b5c357aacda80682643353e9698130a20476ba56
RubenGarEEP <145350414+RubenGarEEP@users.noreply.github.com>
2024-05-16 12:45:08 +0200

    añado un logo y un titulo a la pantalla principal

--- app/src/main/java/com/example/universityextracurricular/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/universityextracurricular/ui/theme/MainScreen.kt
@@ -1,20 +1,55 @@
 package com.example.universityextracurricular.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.universityextracurricular.R
 
 @Composable
 fun MainScreen(navController: NavHostController) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "O'Rey Vini University",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+
+        Image(
+            painter = painterResource(R.drawable.vini),
+            contentDescription = "Logo de la Universidad",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .padding(bottom = 16.dp),
+            contentScale = ContentScale.Fit
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+
         Button(
             onClick = { navController.navigate("sports") },
             modifier = Modifier
